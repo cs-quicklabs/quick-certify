2b6b89a317807c22780f5c743190de92735f8f6c
tarun <tarunsingh@crownstack.com>
2026-01-14 16:45:19 +0530

    fix(ui):signup page

--- apps/frontend/src/app/(auth)/signup/page.tsx
+++ apps/frontend/src/app/(auth)/signup/page.tsx
@@ -68,7 +68,7 @@ export default function SignupPage() {
   return (
     <div className="p-6 sm:p-8 space-y-4 md:space-y-6">
       {/* Header */}
-      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-6">
+      <h1 className="text-xl font-extrabold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-6">
         Register new issuer account
       </h1>
 
