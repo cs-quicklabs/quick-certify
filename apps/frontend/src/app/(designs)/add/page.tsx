5e529cd3ba1c43f8e9e0b810773e414df75407e6
tarun <tarunsingh@crownstack.com>
2026-01-19 12:39:40 +0530

    fix: merge conflict

--- apps/frontend/src/app/(designs)/add/page.tsx
+++
@@ -1,5 +0,0 @@
-import DesignFormPage from '../_components/DesignFormPage';
-
-export default function AddDesignPage() {
-  return <DesignFormPage mode="add" />;
-}
--- apps/frontend/src/hooks/useDesign.tsx
+++ apps/frontend/src/hooks/useDesign.tsx
@@ -1,5 +1,8 @@
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a7d6645 (fix: conflict)
 // import { useEffect, useState } from "react";
 // import {
 //   getDesign,
@@ -7,6 +10,7 @@
 //   updateDesign,
 // } from "@/services/api/design.service";
 // import { DesignPayload, Design } from "@/types/design.types";
+<<<<<<< HEAD
 
 // export function useDesign(id?: string) {
 //   const [data, setData] = useState<Design | null>(null);
@@ -48,37 +52,44 @@ import {
   updateDesign,
 } from "@/services/api/design.service";
 import { DesignPayload, Design } from "@/types/design.types";
+=======
+>>>>>>> a7d6645 (fix: conflict)
 
-export function useDesign(id?: string) {
-  const [data, setData] = useState<Design | null>(null);
-  const [loading, setLoading] = useState(false);
+// export function useDesign(id?: string) {
+//   const [data, setData] = useState<Design | null>(null);
+//   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (!id) return;
+//   useEffect(() => {
+//     if (!id) return;
 
-    setLoading(true);
-    getDesign(id)
-      .then(setData)
-      .finally(() => setLoading(false));
-  }, [id]);
+//     setLoading(true);
+//     getDesign(id)
+//       .then(setData)
+//       .finally(() => setLoading(false));
+//   }, [id]);
 
-  return { data, loading };
-}
+//   return { data, loading };
+// }
 
-export function useSaveDesign(mode: "add" | "edit", id?: string) {
-  const [loading, setLoading] = useState(false);
+// export function useSaveDesign(mode: "add" | "edit", id?: string) {
+//   const [loading, setLoading] = useState(false);
 
-  const save = async (payload: DesignPayload) => {
-    setLoading(true);
-    try {
-      mode === "add"
-        ? await createDesign(payload)
-        : await updateDesign(id!, payload);
-    } finally {
-      setLoading(false);
-    }
-  };
+//   const save = async (payload: DesignPayload) => {
+//     setLoading(true);
+//     try {
+//       mode === "add"
+//         ? await createDesign(payload)
+//         : await updateDesign(id!, payload);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
+<<<<<<< HEAD
   return { save, loading };
 }
 >>>>>>> 54d24e8 (fix: merge conflict)
+=======
+//   return { save, loading };
+// }
+>>>>>>> a7d6645 (fix: conflict)
