76807b254a4d911012f3406cf8c017c731cda8e6
Naman1513 <namangoel@crownstack.com>
2026-01-28 15:19:32 +0530

    Playwright Architecture Setup (#77)
    * Initial Project setup for the Playwright automation
    
    * Updatd the playwright config file and resolve the path issue
    
    * Resolved build issues!
    
    * Reinstall the package.lock.json and fixed the induntation of the files!
    
    * updated the packages
    
    * updated the workflow file
    
    * Implemented the suggestions
    
    * Fixed the formatting
    
    * Resolved merge conflicts
    
    * resolved merge conflicts

---
+++ apps/frontend-e2e/Playwright/pageobjects/RegistrationPage.ts
@@ -0,0 +1,88 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+import { RandomDataGenerator } from '../utils/RandomDataGenerator';
+
+export class RegistrationPage {
+  readonly page: Page;
+  readonly testData: RandomDataGenerator;
+  readonly locator_signupBtn: Locator;
+  readonly locator_firstNameField: Locator;
+  readonly locator_lastNameField: Locator;
+  readonly locator_emailField: Locator;
+  readonly locator_issuerNameField: Locator;
+  readonly locator_issuerWebsiteURLField: Locator;
+  readonly locator_passwordField: Locator;
+  readonly locator_confirmPasswordField: Locator;
+  readonly locator_createNewUserBtn: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.testData = new RandomDataGenerator();
+    this.locator_signupBtn = page.getByRole('link', { name: 'Sign up' });
+    this.locator_firstNameField = page.locator(`#first-name`);
+    this.locator_lastNameField = page.locator(`#last-name`);
+    this.locator_emailField = page.locator(`#your-email`);
+    this.locator_issuerNameField = page.locator(`#issuer-name`);
+    this.locator_issuerWebsiteURLField = page.locator(`#issuer-website-url`);
+    this.locator_passwordField = page.locator(`#password`);
+    this.locator_confirmPasswordField = page.locator(`#confirm-password`);
+    this.locator_createNewUserBtn = page.getByRole(`button`, { name: 'Create New Issuer Account' });
+  }
+
+  async clickOnSignupBtn() {
+    await this.locator_signupBtn.click();
+  }
+
+  async enterUserEmail() {
+    const email = this.testData.generateRandomEmail();
+    await this.locator_emailField.fill(email);
+  }
+
+  async enterPassword(password: string) {
+    await this.locator_passwordField.fill(password);
+  }
+
+  async enterConfirmPassword(password: string) {
+    await this.locator_confirmPasswordField.fill(password);
+  }
+
+  async enterFirstName(firstName: string) {
+    await this.locator_firstNameField.fill(firstName);
+  }
+
+  async enterLastName(lastName: string) {
+    await this.locator_lastNameField.fill(lastName);
+  }
+
+  async enterIssuerName() {
+    const issuerName = this.testData.generateRandomName();
+    await this.locator_issuerNameField.fill(issuerName);
+  }
+
+  async enterIssuerWebsiteURL(issuerURL: string) {
+    await this.locator_issuerWebsiteURLField.fill(issuerURL);
+  }
+
+  async clickOnCreateNewUserBtn() {
+    await this.locator_createNewUserBtn.click();
+  }
+
+  async validateUserRegistration() {
+    await expect(this.page).toHaveURL(/\/dashboard/);
+  }
+
+  async enterRegistrationFormData(
+    firstName: string,
+    lastName: string,
+    issuerURL: string,
+    password: string,
+    confirmPassword: string,
+  ) {
+    await this.enterFirstName(firstName);
+    await this.enterLastName(lastName);
+    await this.enterUserEmail();
+    await this.enterIssuerName();
+    await this.enterIssuerWebsiteURL(issuerURL);
+    await this.enterPassword(password);
+    await this.enterConfirmPassword(confirmPassword);
+  }
+}
